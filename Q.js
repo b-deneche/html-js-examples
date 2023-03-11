a97707c96c981580cf15cca47901dc2b591807d8
b-deneche <denechebadreddine@gmail.com>
2023-03-12 00:08:20 +0100

    Questions

--- Q.js
+++ Q.js
@@ -1,5 +1,6 @@
 
 const btns = document.querySelectorAll('.btn');
+<<<<<<< HEAD
 const qas = document.querySelector('.qas');
 
 btns.forEach((e)=>{
@@ -9,3 +10,13 @@ btns.forEach((e)=>{
     })
 })
 
+=======
+
+btns.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        const qas = e.parentElement.parentElement;
+        const a = qas.querySelector('.answer').classList.toggle('show-answer');    
+    })
+    
+})
+>>>>>>> 5eb3ba6 (Questions)
