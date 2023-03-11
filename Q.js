0fa6bfd64cbde28e8abb4163c81f9fe217fff798
b-deneche <denechebadreddine@gmail.com>
2023-03-12 00:08:54 +0100

    menu - video backgrnd

--- Q.js
+++ Q.js
@@ -1,6 +1,7 @@
 
 const btns = document.querySelectorAll('.btn');
 <<<<<<< HEAD
+<<<<<<< HEAD
 const qas = document.querySelector('.qas');
 
 btns.forEach((e)=>{
@@ -11,12 +12,20 @@ btns.forEach((e)=>{
 })
 
 =======
+=======
+const qas = document.querySelector('.qas');
+>>>>>>> e44f16f (menu - video backgrnd)
 
 btns.forEach((e)=>{
     e.addEventListener('click',()=>{
-        const qas = e.parentElement.parentElement;
-        const a = qas.querySelector('.answer').classList.toggle('show-answer');    
+        //  const qas = e.parentElement.parentElement;
+         const a = qas.querySelector('.answer').classList.toggle('show-answer');    
     })
+<<<<<<< HEAD
     
 })
 >>>>>>> 5eb3ba6 (Questions)
+=======
+})
+
+>>>>>>> e44f16f (menu - video backgrnd)
